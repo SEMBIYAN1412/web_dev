fc4c6a98962deab01538db1a7919efb4b1136c7e
SEMBIYAN1412 <20cab247sembiyan@gmail.com>
2024-04-06 22:45:02 +0530

    addding javascript

---
+++ javascript/js/main.js
@@ -0,0 +1,8 @@
+//console.log("hello js learners")
+//console.log(typeof ("dhuddu"))
+//console.log(typeof(90))
+
+myVariable="Mathematics";
+//length
+console.log(myVariable)
+console.log(myVariable.length)
