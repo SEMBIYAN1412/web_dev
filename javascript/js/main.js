813ed858edbc4d99ea9245576c2f7000337d5b59
SEMBIYAN1412 <20cab247sembiyan@gmail.com>
2024-04-07 20:48:54 +0530

    learn string methods

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -6,3 +6,24 @@ myVariable="Mathematics";
 //length
 console.log(myVariable)
 console.log(myVariable.length)
+//charAt()
+console.log(myVariable.charAt(5))
+
+//indexof
+console.log(myVariable.indexOf("at"))
+console.log(myVariable.lastIndexOf("at"))
+
+//slice()
+console.log(myVariable.slice(5,8))
+
+//upppercase lowercase()
+console.log(myVariable.toUpperCase())
+console.log(myVariable.toLowerCase())
+
+//includes() <return boolean values>
+console.log(myVariable.includes("mat"))
+
+//split() <returns array include elements>
+console.log(myVariable.split('e'))
+console.log('subscribers'.split('b'))
+
