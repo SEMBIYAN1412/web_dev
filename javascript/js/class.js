5695499f749bff3539c7f6f174915417c95cd795
SEMBIYAN1412 <20cab247sembiyan@gmail.com>
2024-05-02 19:35:09 +0530

    learn classes

--- javascript/js/class.js
+++ javascript/js/class.js
@@ -1,17 +1,25 @@
 //class
 
 class mypizza{
-    constructor(typepizza,sizepizza,crustpizza,pizzatoppigs){
+    constructor(typepizza,sizepizza,crustpizza){
     this.type=typepizza;
     this.size=sizepizza;
     this.crust=crustpizza;
-    this.topppings=pizzatoppigs
+    this.topppings="olives";
 }
-bake(){
-    return console.log(`Baking a ${this.type} ${this.size} size ${this.crust} pizza with ${this.topppings} toppings`);
-}
+    bake(){
+        return console.log(`Baking a ${this.type} ${this.size} size ${this.crust} pizza with ${this.topppings} toppings`);
+    }
+    getToppings(){
+        return this.topppings;
+    }
+    setToppings(toppingspizza){
+        this.topppings=toppingspizza;
+    }
 }
 
 //object
-const anoterpizza=new mypizza("mirgarita","small","thin","olives");
+const anoterpizza=new mypizza("mirgarita","small","thin");
+anoterpizza.setToppings("sassuage");
 anoterpizza.bake();
+console.log(anoterpizza.getToppings());
