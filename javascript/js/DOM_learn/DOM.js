4eaa5dcd1b8246fc7c7d6ae77d5dfdfc8038c56b
SEMBIYAN1412 <20cab247sembiyan@gmail.com>
2024-05-31 21:39:59 +0530

    learn DOM

--- javascript/js/DOM_learn/DOM.js
+++ javascript/js/DOM_learn/DOM.js
@@ -5,7 +5,6 @@ console.log(view1);
 
 const view2=document.querySelector("#view2");
 console.log(view2);
-
 //overwrite
 view1.style.display="flex";
 view2.style.display="null";
@@ -18,3 +17,15 @@ console.log(sameview);
 
 const divs= document.querySelectorAll("div");
 console.log(divs);
+
+const tagview=view1.getElementsByTagName("div");
+console.log(tagview);
+
+const evenDivs=view1.querySelectorAll("div:nth-of-type(2n)");
+console.log(evenDivs);
+evenDivs[3].style.backgroundColor="red";
+
+for (i=0; i<evenDivs.length; i++){
+    evenDivs[i].style.width="180px";
+    evenDivs[i].style.backgroundColor="skyblue";
+}
