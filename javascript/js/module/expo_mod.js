c46da1a6d91a64eb71d2161a5df27b003cef5f77
SEMBIYAN1412 <20cab247sembiyan@gmail.com>
2024-06-07 19:28:49 +0530

    learn module

---
+++ javascript/js/module/expo_mod.js
@@ -0,0 +1,22 @@
+// export default function earnMoney(){
+//     return "earn money by  using from our learnings";
+// }
+// export const growMoney=()=>{
+//     return "grow money from our earnings";
+// }
+// export const giveMoney=()=>{
+//     return "give our excess money";
+// }
+
+// //export default{earnMoney}
+// //export{growMoney,GiveMoney}
+
+export default class subscriber{
+    constructor(name){
+        this._name=name;
+    }
+    email(){
+        return `hii i am ${this.name} .emiled to sanjay@gmail.com`
+    }
+
+}
---
+++ javascript/js/module/import_mod.js
@@ -0,0 +1,9 @@
+// import earnMoney from "./expo_mod";
+// import { growMoney,giveMoney } from "./expo_mod";
+// console.log(earnMoney());
+// console.log(growMoney());
+// console.log(giveMoney());
+
+import subscriber from "./expo_mod";
+const me=new subscriber("sanjay");
+console.log(me.email()); 
